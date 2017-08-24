a8be778ec41bd93ba3a010afc289ebb495de4adc
womd <chk.mailbox@gmail.com>
2017-08-24 11:51:39 +0200

    initial checkin

---
+++ ControllerWithDefaultGridCRUD/ControllerWithDefaultGridCRUD/UI/CustomViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNet.Scaffolding;
+using Microsoft.AspNet.Scaffolding.EntityFramework;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ControllerWithDefaultGridCRUD.UI
+{
+    /// <summary>
+    /// View model for code types so that it can be displayed on the UI.
+    /// </summary>
+    public class CustomViewModel
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="context">The code generation context</param>
+        public CustomViewModel(CodeGenerationContext context)
+        {
+            Context = context;
+        }
+
+        /// <summary>
+        /// This gets all the Model types from the active project.
+        /// </summary>
+       
+        public CodeGenerationContext Context { get; private set; }
+
+        public string NameSpace { get; set; }
+        public string ControllerName { get; set; }
+        public string ViewModel { get; set; }
+        public string GridName { get; set; }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # MVC_Scraffolders
 Some VS Scraffolders for MVC
+
+at this stage it contains an Controller-Scraffolder With CRUD - Operations for Kendo-Grid in Batch Mode
